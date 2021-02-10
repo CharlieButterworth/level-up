9a062acce7e215f4002fc99adf999fa00c12640c
Charles Butterworth <charlessbutterworth@gmail.com>
2021-02-10 15:27:18 -0600

    finished ch8

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -8,4 +8,3 @@ class Event(models.Model):
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
     event_time = models.DateTimeField(auto_now=False, auto_now_add=False)
     location = models.CharField(max_length=50)
-    date = models.DateField((""), auto_now=False, auto_now_add=False)
--- levelupapi/views/__init__.py
+++ levelupapi/views/__init__.py
@@ -2,3 +2,4 @@ from .auth import login_user
 from .auth import register_user
 from .gametypes import GameTypes
 from .games import GamesView
+from .event import Events
